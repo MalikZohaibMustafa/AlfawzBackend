ec5078e19226d3c5faf1acf298c362c58dcd314f
Malik <zohaibmalik964242@gmail.com>
2023-09-19 22:59:56 +0500

    Backend with Cors

---
+++ urls.js
@@ -0,0 +1,8 @@
+const BASEURL = "https://alfawzbackend.vercel.app"
+const DATABASE = "mongodb+srv://Alfawz:2TrSj5WFaWfGtmiw@alfawz.hkhcfgl.mongodb.net/portal"
+const SECRET_KEY = 'thisIsthetokenkeyforusertoconnect'
+module.exports = {
+    BASEURL,
+    DATABASE,
+    SECRET_KEY
+};
