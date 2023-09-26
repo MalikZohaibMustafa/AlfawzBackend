aacae2c6770f1689cf8099a4447623a06cb0551d
MALIK ZOHAIB MUSTAFA <56153394+MalikZohaibMustafa@users.noreply.github.com>
2023-09-26 23:32:34 +0500

    Update urls.js

--- urls.js
+++ urls.js
@@ -1,4 +1,4 @@
-const BASEURL = "https://alfawz.up.railway.app/"
+const BASEURL = "gold-shiny-cormorant.cyclic.cloud/"
 //const BASEURL = "http://localhost:3006/"
 const DATABASE = "mongodb+srv://Alfawz:2TrSj5WFaWfGtmiw@alfawz.hkhcfgl.mongodb.net/portal"
 const SECRET_KEY = 'thisIsthetokenkeyforusertoconnect'
