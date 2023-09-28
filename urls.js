f011bdcec8f50c946c88e72f059de94e8292e6b4
MALIK ZOHAIB MUSTAFA <56153394+MalikZohaibMustafa@users.noreply.github.com>
2023-09-29 01:04:51 +0500

    Update urls.js

--- urls.js
+++ urls.js
@@ -1,5 +1,5 @@
 // const BASEURL = "https://alfawzbackend.onrender.com/"
-//const BASEURL = "https://alfawz.up.railway.app/"
+const BASEURL = "https://alfawz.up.railway.app/"
 const DATABASE = "mongodb+srv://Alfawz:2TrSj5WFaWfGtmiw@alfawz.hkhcfgl.mongodb.net/portal"
 const SECRET_KEY = 'thisIsthetokenkeyforusertoconnect'
 module.exports = {
